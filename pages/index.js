dcb279e003fa9c96643d3c81b998ec4e50946fc2
Callan Murphy <callanmurphy31@gmail.com>
2021-11-18 17:39:36 -0500

    ned colour, styling and buttons added

--- pages/index.js
+++ pages/index.js
@@ -8,10 +8,11 @@ import ned1 from '../data/ned/week1.json';
 import ned2 from '../data/ned/week2.json';
 import ned3 from '../data/ned/week3.json';
 import ned4 from '../data/ned/week4.json';
+import style1 from '../styles/burwash.module.css';
+import style2 from '../styles/ned.module.css';
 
 const weeks = [[week1, ned1], [week2, ned2], [week3, ned3], [week4, ned4]];
 const days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
-const colour = '#A30031'
 
 class Home extends Component {
 
@@ -19,7 +20,7 @@ class Home extends Component {
     super(props);
     this.state = {
       date: new Date(),
-      menu: 0
+      menu: {name: 'burwash', index: 0, style: style1, colour: '#A30031'}
     };
   }
 
@@ -51,6 +52,7 @@ class Home extends Component {
   render() {
 
     const today = this.state.date;
+    const menu = this.state.menu;
 
     return (
       <div className="container">
@@ -60,6 +62,7 @@ class Home extends Component {
           <meta name="description" content="Burwash Dining Hall Menu (2021-2022) – Victoria University, Toronto ON. 
           See what's on for dinner by clicking through the daily schedule." />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta name="theme-color" content={this.state.menu.colour} />
           <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
           <link rel="apple-touch-startup-image" href="/splash.png" />
           <link rel="icon" href="/favicon.ico" />
@@ -82,28 +85,28 @@ class Home extends Component {
               <h2>Lunch and Dinner menu has swapped for today! (Dinner: Indian Butter Chicken)</h2>
             </div>
           </div> */}
-          <div className={this.state.menu == 1 ? 'nav-bar green' : 'nav-bar'}>
+          <div className={menu.style['nav-bar']}>
             <a href="." >
-              <img src="/crest-outlined.png" alt="Vic Crest" className="logo-top" />
+              <img src={menu.name == 'burwash' ? '/crest-outlined.png' : '/crest-outlined-green.png'} alt="Vic Crest" className="logo-top" />
             </a>
           </div>
           <div>
-            <h1 className='title'>Burwash Menu</h1>
+            <h1 className='title'>{menu.name == 'burwash' ? 'Burwash' : 'Ned\'s'} Menu</h1>
             <h2 className='title2'>Week {this.getWeek()}</h2>
             <div className='row-container'>
-              <button onClick={() => this.setState({menu: this.state.menu == 0 ? 1 : 0 })} className={'arrow'}>
-                <p>{this.state.menu == 1 ? 'Burwash Menu' : 'Ned\'s Menu'}</p>
+              <button onClick={() => this.setState({menu: menu.name == 'burwash' ? {name: 'ned', index: 1, style: style2, colour: '#58ad60'} : {name: 'burwash', index: 0, style: style1, colour: '#A30031'} })} className={menu.style['swap']}>
+                <p>{menu.name == 'ned' ? 'Burwash Menu' : 'Ned\'s Menu'}</p>
               </button>
             </div>
             <div className='row-container'>
-              <button onClick={() => this.changeDate('backward')} className='arrow'>
-                <img src="/arrow.svg" alt="Left Arrow" className={this.state.menu == 1 ? 'left-arrow green' : 'left-arrow'} />
+              <button onClick={() => this.changeDate('backward')} className={menu.style['arrow']}>
+                <img src="/arrow.svg" alt="Left Arrow" className={'left-arrow'} />
               </button>
-              <h2 className={today.getDate() == new Date().getDate() && today.getMonth() == new Date().getMonth() ? 'subtitle' : 'subtitle other-day' }>
+              <h2 className={today.getDate() == new Date().getDate() && today.getMonth() == new Date().getMonth() ? menu.style['subtitle'] : menu.style['subtitle'] + ' other-day' }>
                 {today.toDateString().replace('2021', '').replace('2022', '')}
               </h2>
-              <button onClick={() => this.changeDate('forward')} className='arrow'>
-                <img src="/arrow.svg" alt="Right Arrow" className={this.state.menu == 1 ? 'right-arrow green' : 'right-arrow'} />
+              <button onClick={() => this.changeDate('forward')} className={menu.style['arrow']}>
+                <img src="/arrow.svg" alt="Right Arrow" className={'right-arrow'} />
               </button>
             </div>
             {/* <div className='row-container'>
@@ -121,8 +124,8 @@ class Home extends Component {
                     <b>Dinner Only (first meal of 2022)</b>
                   </div>
                 }
-                <table className='mainTable'>
-                { weeks[this.getWeek() - 1][this.state.menu].map((col, index) => 
+                <table className={'mainTable ' + menu.style['mainTable']}>
+                { weeks[this.getWeek() - 1][menu.index].map((col, index) => 
                   ((col[this.formatDay(today.getDay())] != "" && col['Victoria University Food Services Burwash Dining Hall']) != "" || col['Victoria University Food Services Burwash Dining Hall'] == "DINNER") ?
                       ((col['Victoria University Food Services Burwash Dining Hall'] == "WEEK " + this.getWeek()) || col['Victoria University Food Services Burwash Dining Hall'] == "DINNER" ) ? 
                       <tr>
@@ -139,7 +142,7 @@ class Home extends Component {
                     )
                 }
                 </table>
-                <div>
+                <div id='schedule'>
                   { today.getDay() == 0 || today.getDay() == 6 ?
                     <div className="title2 space-above">
                       <b>Saturday, Sunday & Holidays</b>
@@ -155,6 +158,11 @@ class Home extends Component {
                     </div>
                   }
                 </div>
+                <div className='row-container'>
+              <button onClick={() => {window.scrollTo(0,document.body.scrollHeight + 500); this.setState({menu: menu.name == 'burwash' ? {name: 'ned', index: 1, style: style2, colour: '#58ad60'} : {name: 'burwash', index: 0, style: style1, colour: '#A30031'} })}} className={menu.style['swap']}>
+                <p>{menu.name == 'ned' ? 'Burwash Menu' : 'Ned\'s Menu'}</p>
+              </button>
+            </div>
               </div>
             }
           </div>
@@ -162,34 +170,25 @@ class Home extends Component {
 
         <footer>
           <a href="https://vicu.utoronto.ca/" target="_blank" rel="noopener noreferrer">
-            <img src="/crest.png" alt="Vic Crest" className="logo" />
+            <img src={menu.name == 'burwash' ? '/crest.png' : '/crest-green.png'} alt="Vic Crest" className="logo" />
             {' '}Abeunt Studia in Mores
           </a>
         </footer>
 
-        <style jsx>{`
+        <style jsx global>{`
+          html,
+          body {
+            padding: 0;
+            margin: 0;
+            font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+              Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+              sans-serif;
+          }
+
           .heading {
             font-weight: 500;
           }
-
-          .nav-bar {
-            position: absolute;
-            /* position: fixed; */
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            top: 0px;
-            left: 0px;
-            right: 0px;
-            width: 100%;
-            background: #A30031;
-            height: 80px;
-            z-index: 10;
-            -webkit-box-shadow: 0px 0px 6px 3px rgba(41,41,41,.25);
-            -moz-box-shadow: 0px 0px 6px 3px rgba(41,41,41,.25);
-            box-shadow: 0px 0px 6px 3px rgba(41,41,41,.25);
-          }
-
+          
           .alert-bar {
             position: absolute;
             position: fixed;
@@ -202,7 +201,7 @@ class Home extends Component {
             right: 0px;
             width: 100%;
             background: #FFFF60;
-            // height: 50px;
+            /* height: 50px; */
             z-index: 20;
             padding-left: 50px;
             padding-right: 50px;
@@ -212,16 +211,11 @@ class Home extends Component {
             -moz-box-shadow: 0px 0px 6px 3px rgba(41,41,41,.25);
             box-shadow: 0px 0px 6px 3px rgba(41,41,41,.25);
           }
-
+          
           .alert-bar h2 {
             font-size: 16px;
           }
-
-          // .inline {
-          //   display: flex;
-          //   align-items: center;
-          // }
-
+          
           main {
             background-color: #ffffff;
             font-family: -apple-system, BlinkMacSystemFont, sans-serif;
@@ -229,40 +223,33 @@ class Home extends Component {
             padding-bottom: 40px;
             margin: 0 4%;
           }
-
+          
           .title {
             text-align: center;
             font-weight: 400;
           }
-
+          
           .title2 {
             text-align: center;
             font-weight: 300;
           }
-
+          
           .center {
             text-align: center;
           }
-
+          
           .space-above {
             padding-top: 45px;
           }
-
+          
           .space-below {
             padding-bottom: 40px;
           }
-
+          
           h2 {
             font-size: 26px
           }
-
-          .subtitle {
-            text-align: center;
-            width: 200px;
-            color: #A30031;
-            font-weight: 400;
-          }
-
+          
           .other-day {
             color: black;
           }
@@ -276,72 +263,49 @@ class Home extends Component {
             font-weight: 300;
           }
 
-          .mainTable th {
-            background-color: #A30031;
-            height: 50px;
-            color: #ffffff;
-          }
-
           .mainTable tr {
             background-color: #dfdfdf;
             color: #161616;
             width: 100%;
             overflow: hidden;
           }
-
+          
           .mainTable th,
           .mainTable td {
             padding: 12px 15px;
           }
-
+          
           .mainTable th:first-child {
             border-radius: 10px 0 0 0px;
           }
-
+          
           .mainTable th:last-child {
             border-radius: 0 10px 0px 0;
           }
-
+          
           .mainTable tr:nth-of-type(even) {
             background-color: #f7f7f7;
           }
-
+          
           .table-newline {
             white-space: pre-line;
             width: 70%;
           }
-
-          .green {
-            background-color: #58ad60
-          }
-
-
+          
           .row-container {
             justify-content: center;
             text-align: center;
             display: flex;
             flex-direction: row;
-            // margin-bottom: 30px;
+            /* // margin-bottom: 30px; */
           }
-
-          .arrow {
-            background-color: #A30031;
-            border-radius: 10px;
-            border: none;
-            align-self: flex-start;
-            color: white;
-            padding: 8px 18px;
-            font-size: 20px;
-            cursor: pointer;
-            margin-top: 10px;
-          }
-
+          
           .today {
             margin-top: -30px;
             opacity: 30%;
             cursor: auto;
           }
-
+          
           .not-today {
             margin-top: -30px;
             opacity: 100%;
@@ -350,11 +314,7 @@ class Home extends Component {
           .undo {
             width: 40px;
           }
-
-          .arrow:hover {
-            background-color: #7A0025;
-          }
-
+          
           footer {
             font-weight: 300;
             width: 100%;
@@ -364,59 +324,40 @@ class Home extends Component {
             justify-content: center;
             align-items: center;
           }
-
-          footer img {
-            // margin-left: 0.5rem;
-          }
-
+          
           footer a {
             display: flex;
             align-items: center;
-            justify-content center;
+            justify-content: center;
           }
-
+          
           a {
             color: inherit;
             text-decoration: none;
           }
-
+          
           .logo-top {
             height: 2.5em;
           }
-
+          
           .right-arrow {
             height: 1.7em;
           }
-
+          
           .left-arrow {
             height: 1.7em;
             transform: rotate(180deg);
           }
-
+          
           .logo {
             height: 1.5em;
             margin-right: 12px;
           }
-
+          
           @media screen and (min-width: 0px) and (max-width: 650px) {
-            // .arrow {
-            //   align-self: none;
-            //   color: blue;
-            // }
             .mainTable {
               width: 98%;
             }
-          }
-        `}</style>
-
-        <style jsx global>{`
-          html,
-          body {
-            padding: 0;
-            margin: 0;
-            font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-              Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-              sans-serif;
           }
 
           * {
