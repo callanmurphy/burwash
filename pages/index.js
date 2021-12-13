e3031e76fc80073a5a49a417177036ad3dea6b64
callan <callan@callans-MacBook-Pro.local>
2021-12-12 20:39:37 -0500

    slider start

--- pages/index.js
+++ pages/index.js
@@ -94,8 +94,11 @@ class Home extends Component {
             <h1 className='title'>{menu.name == 'burwash' ? 'Burwash' : 'Ned\'s'} Menu</h1>
             <h2 className='title2'>Week {this.getWeek()}</h2>
             <div className='row-container'>
-              <button onClick={() => this.setState({menu: menu.name == 'burwash' ? {name: 'ned', index: 1, style: style2, colour: '#58ad60'} : {name: 'burwash', index: 0, style: style1, colour: '#A30031'} })} className={menu.style['swap']}>
-                <p>{menu.name == 'ned' ? 'Burwash Menu' : 'Ned\'s Menu'}</p>
+              <button onClick={() => this.setState({menu: menu.name == 'burwash' ? {name: 'ned', index: 1, style: style2, colour: '#58ad60'} : {name: 'burwash', index: 0, style: style1, colour: '#A30031'} })} className={style1['swap']}>
+                <p>{'Burwash Menu'}</p>
+              </button>
+              <button onClick={() => this.setState({menu: menu.name == 'burwash' ? {name: 'ned', index: 1, style: style2, colour: '#58ad60'} : {name: 'burwash', index: 0, style: style1, colour: '#A30031'} })} className={style2['swap']}>
+                <p>{'Ned\'s Menu'}</p>
               </button>
             </div>
             {/* { menu.name == 'burwash' && <img src="/new.svg" alt="NEW" className={'new-ribbon'} />} */}
