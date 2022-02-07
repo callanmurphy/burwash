59ba88a744d17fb884cf84091724013457863a3b
callan <callanmurphy31@gmail.com>
2022-02-07 11:25:52 -0500

    ned hot food is back message added and strikethrough removed

--- pages/index.js
+++ pages/index.js
@@ -181,7 +181,7 @@ class Home extends Component {
             : <div>
                 { menu.name == 'ned' && !(today.getDay() == 0 || today.getDay() == 6) &&
                   <div className='center space-below'>
-                    <b>*Ned's Menu is subject to change*</b>
+                    <b>Ned's entrees are back :D</b>
                   </div>
                 }
                 {/* 
@@ -202,11 +202,11 @@ class Home extends Component {
                         <td className='heading new'>{col['title']}</td>
                         <td className='table-newline new'>{col[this.formatDay(today.getDay())]}</td>
                       </tr>
-                      : ((col['title'] == "MEAT ENTREE" || col['title'] == "VEGETABLE ENTREE" || col['title'] == "SIDES") && menu.name == 'ned') ?
-                      <tr>
-                        <td className='heading strike'>{col['title']}</td>
-                        <td className='table-newline strike'>{col[this.formatDay(today.getDay())]}</td>
-                      </tr>
+                      // : ((col['title'] == "MEAT ENTREE" || col['title'] == "VEGETABLE ENTREE" || col['title'] == "SIDES") && menu.name == 'ned') ?
+                      // <tr>
+                      //   <td className='heading strike'>{col['title']}</td>
+                      //   <td className='table-newline strike'>{col[this.formatDay(today.getDay())]}</td>
+                      // </tr>
                       :
                       <tr>
                         <td className='heading'>{col['title']}</td>
